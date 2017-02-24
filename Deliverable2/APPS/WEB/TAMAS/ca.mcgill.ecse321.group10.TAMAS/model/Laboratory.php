3fb1968e29456889876c89fc4130c067009e4ba8
Camilo Garcia La Rotta <camilo.garcia.larotta@gmail.com>
2017-02-24 00:13:00 -0500

    Organized Desktop and Web app under APPS directory - where did it all go wrong

---
+++ Deliverable2/APPS/WEB/TAMAS/ca.mcgill.ecse321.group10.TAMAS/model/Laboratory.php
@@ -0,0 +1,36 @@
+<?php
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.25.0-9e8af9e modeling language!*/
+
+class Laboratory extends Job
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public function __construct($aStartTime, $aEndTime, $aSalary, $aRequirements, $aCourse, $aInstructor)
+  {
+    parent::__construct($aStartTime, $aEndTime, $aSalary, $aRequirements, $aCourse, $aInstructor);
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public function equals($compareTo)
+  {
+    return $this == $compareTo;
+  }
+
+  public function delete()
+  {
+    parent::delete();
+  }
+
+}
+?>
