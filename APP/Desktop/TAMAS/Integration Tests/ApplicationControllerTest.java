2841a74a49f658eb0b8758de62ac77167120ffd1
jshnaidman <jshnaidman245@gmail.com>
2017-03-26 13:32:16 -0400

    changed my work directory and added the test files to github

---
+++ APP/Desktop/TAMAS/Integration Tests/ApplicationControllerTest.java
@@ -0,0 +1,97 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.sql.Time;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.mcgill.ecse321.group10.TAMAS.model.ApplicationManager;
+import ca.mcgill.ecse321.group10.TAMAS.model.Course;
+import ca.mcgill.ecse321.group10.TAMAS.model.Instructor;
+import ca.mcgill.ecse321.group10.TAMAS.model.Job;
+import ca.mcgill.ecse321.group10.TAMAS.model.ProfileManager;
+import ca.mcgill.ecse321.group10.TAMAS.model.Student;
+import ca.mcgill.ecse321.group10.controller.ApplicationController;
+import ca.mcgill.ecse321.group10.controller.InputException;
+import ca.mcgill.ecse321.group10.controller.ProfileController;
+import ca.mcgill.ecse321.group10.persistence.PersistenceXStream;
+
+public class ApplicationControllerTest {
+	private ApplicationController ac;
+	private ApplicationManager am;
+	private ProfileController pc;
+	private ProfileManager pm;
+	private static String outputFile1 = "output" + File.separator + "tests" + File.separator + "testApplicationController";
+	private static String outputFile2 = "output" + File.separator + "tests" + File.separator + "testProfileController";;
+	private static Time timeBefore = new Time(10);
+	private static Time timeAfter = new Time(100);
+
+
+	@Before
+	public void setUp() throws Exception {
+		PersistenceXStream.initializeProfileManager(outputFile2);
+		PersistenceXStream.initializeApplicationManager(outputFile1, outputFile2);
+		am = new ApplicationManager();
+		ac = new ApplicationController(am, outputFile1);
+		assertEquals(0,am.getApplications().size());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		am.delete();
+		File f1 = new File(outputFile1);
+		File f2 = new File(outputFile2);
+		f1.delete();
+		f2.delete();
+		assertEquals(0,am.getApplications().size());
+	}
+
+	@Test
+	public void testModifyJobAndCreateApplication() throws InputException {
+		Course aCourse = new Course("testCourse",1,2,3);
+		Instructor aInstructor = new Instructor("User","Pass", "first", "Last");
+		ac.addJobToSystem(timeBefore, timeAfter, "Monday", 1000, "None", aCourse, aInstructor);
+		
+		am = (ApplicationManager) PersistenceXStream.loadFromXMLwithXStream();
+		assertEquals(1,am.getJobs().size());
+		
+		ac.modifyJobPosition(0, Job.Position.TA);
+		am = (ApplicationManager) PersistenceXStream.loadFromXMLwithXStream();
+		assertEquals(Job.Position.TA, am.getJob(0).getPosition());
+		
+		Student aStudent = new Student("user", "pass", "James", "McGill", "exp");
+		
+		ac.createApplication(aStudent, am.getJob(0));
+		am = (ApplicationManager) PersistenceXStream.loadFromXMLwithXStream();
+		assertEquals(1,am.getApplications().size());
+	}
+	
+	@Test
+	public void testAddJobToSystemInvalid(){
+		Course aCourse = new Course("testCourse",1,2,3);
+		Instructor aInstructor = new Instructor("User","Pass", "first", "Last");
+		
+		try {
+			ac.addJobToSystem(timeBefore, timeAfter, "Monday", 1000, "None", null, aInstructor);
+		} catch (InputException e) {
+			am = (ApplicationManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0,am.getApplications().size());
+		}
+		
+		try {
+			ac.addJobToSystem(timeBefore, timeAfter, "Monday", 1000, "None", aCourse, null);
+		} catch (InputException e) {
+			am = (ApplicationManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0,am.getApplications().size());
+		}
+		try {
+			ac.addJobToSystem(timeAfter, timeBefore, "Monday", -1000, "None", aCourse, aInstructor);
+		} catch (InputException e) {
+			am = (ApplicationManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0,am.getApplications().size());
+		}
+	}
+}
+
---
+++ APP/Desktop/TAMAS/Integration Tests/CourseControllerTest.java
@@ -0,0 +1,93 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.mcgill.ecse321.group10.TAMAS.model.CourseManager;
+import ca.mcgill.ecse321.group10.controller.CourseController;
+import ca.mcgill.ecse321.group10.controller.InputException;
+import ca.mcgill.ecse321.group10.persistence.PersistenceXStream;
+
+public class CourseControllerTest {
+	private CourseController cc;
+	private CourseManager cm;
+	private static String outputFile = "output" + File.separator + "tests" + File.separator + "testCourseController";
+
+
+	@Before
+	public void setUp() throws Exception {
+		PersistenceXStream.initializeCourseManager(outputFile);
+		cm = new CourseManager();
+		cc = new CourseController(cm, outputFile);
+		
+		assertEquals(0,cm.getCourses().size());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		cm.delete();
+		File f = new File(outputFile);
+		f.delete();
+		assertEquals(0,cm.getCourses().size());
+	}
+
+	@Test
+	public void testCreateCourseValid() throws InputException {
+		cc.createCourse("history", 1, 2, 3);
+		cm = (CourseManager) PersistenceXStream.loadFromXMLwithXStream();
+		assertEquals(1,cm.getCourses().size());
+	}
+	
+	@Test
+	public void testCreateCourseInvalid(){
+		try{
+			cc.createCourse(null, 1, 2, 3);
+		}catch(InputException e){
+			cm = (CourseManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0,cm.getCourses().size());
+		}
+		try{
+			cc.createCourse("", 1, 2, 3);
+		}catch(InputException e){
+			cm = (CourseManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0,cm.getCourses().size());
+		}
+		try{
+			cc.createCourse("course", -1, 2, 3);
+		}catch(InputException e){
+			cm = (CourseManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0,cm.getCourses().size());
+		}
+		try{
+			cc.createCourse("course", 1, -2, 3);
+		}catch(InputException e){
+			cm = (CourseManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0,cm.getCourses().size());
+		}
+		try{
+			cc.createCourse("course", 1, 2, -3);
+		}catch(InputException e){
+			cm = (CourseManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0,cm.getCourses().size());
+		}
+	}
+	
+	@Test
+	public void testUniqueCDN(){
+		try {
+			cc.createCourse("course1", 1,2,3);
+		} catch (InputException e) {
+		}
+		
+		try {
+			cc.createCourse("course2", 1,4,5);
+		} catch (InputException e) {
+			cm = (CourseManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(1,cm.getCourses().size());
+		}
+	}
+
+}
