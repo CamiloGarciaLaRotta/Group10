7a64400cd195ca938171a37e57bceb56792d2208
jshnaidman <jshnaidman245@gmail.com>
2017-03-26 11:59:25 -0400

    finished profile controller tests

--- APP/Desktop/TAMAS/Integration Tests/ProfileControllerTest.java
+++ APP/Desktop/TAMAS/Integration Tests/ProfileControllerTest.java
@@ -47,8 +47,14 @@ public class ProfileControllerTest {
 		pc.addInstructorToSystem("Boogie", "Goodman", "Joseph", "Stalin");
 		pc.addAdminToSystem("MartinLutherKing", "Was not a man", "Of Tolerance", "But Social Revolution");
 		pc.addStudentToSystem("Seize", "The", "Means", "Of", "Production");
+		pc.addStudentToSystem("exp", "is", "blank", "test", "");
+		
+		
+		Course course = new Course("history", 1,2,3);
+		pc.addCourseToInstructor(0, course);
 		
 		pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+		
 		
 		assertEquals(pm.getInstructor(0).getFirstName(), "Joseph");
 		assertEquals(pm.getAdmin(0).getUsername(), "MartinLutherKing");
@@ -65,15 +71,115 @@ public class ProfileControllerTest {
 			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
 			assertEquals(0, pm.getInstructors().size());
 		}
+		try {
+			pc.addInstructorToSystem("", "eh", "guy", "Jones");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0, pm.getInstructors().size());
+		}
+		try {
+			pc.addInstructorToSystem("man", "", "guy", "Jones");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0, pm.getInstructors().size());
+		}
+		try {
+			pc.addInstructorToSystem("man", "eh", "", "Jones");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertEquals(0, pm.getInstructors().size());
+			
+		}
 		
+		try {
+			pc.addAdminToSystem(null, "eh", "guy", "Jones");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getAdmins().size() == 0);
+		}
 		try {
 			pc.addAdminToSystem("", "eh", "guy", "Jones");
 		} catch (InputException e) {
 			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
 			assertTrue(pm.getAdmins().size() == 0);
 		}
+		try {
+			pc.addAdminToSystem("man", "", "guy", "Jones");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getAdmins().size() == 0);
+		}
+		try {
+			pc.addAdminToSystem("man", "eh", "", "Jones");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getAdmins().size() == 0);
+		}
+		
+		try {
+			pc.addStudentToSystem(null, "eh", "guy", "Jones", "abc");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getStudents().size() == 0);
+		}
+		try {
+			pc.addStudentToSystem("", "eh", "guy", "Jones", "abc");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getStudents().size() == 0);
+		}
+		try {
+			pc.addStudentToSystem("man", "", "guy", "Jones", "abc");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getStudents().size() == 0);
+		}
+		try {
+			pc.addStudentToSystem("man", "eh", "", "Jones", "abc");
+		} catch (InputException e) {
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getStudents().size() == 0);
+		}
 		
 
+	}
+	
+	@Test
+	public void testUsernameTaken() {
+		try {
+			pc.addStudentToSystem("user1", "pass","first","last", "exp");
+		} catch (InputException e1) {
+			// TODO Auto-generated catch block
+		}
+		
+		try{
+			pc.addStudentToSystem("user1", "pass","first","last", "exp");
+		} catch(Exception e){
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getStudents().size() == 1);
+		}
+		
+		try{
+			pc.addInstructorToSystem("user2", "pass","first","last");
+		} catch(Exception e){
+		}
+		
+		try{
+			pc.addInstructorToSystem("user2", "pass","first","last");
+		} catch(Exception e){
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getInstructors().size() == 1);
+		}
+		try{
+			pc.addAdminToSystem("user3", "pass","first","last");
+		} catch(Exception e){
+		}
+		try{
+			pc.addAdminToSystem("user3", "pass","first","last");
+		} catch(Exception e){
+			pm = (ProfileManager) PersistenceXStream.loadFromXMLwithXStream();
+			assertTrue(pm.getAdmins().size() == 1);
+		}
 	}
 
 }
